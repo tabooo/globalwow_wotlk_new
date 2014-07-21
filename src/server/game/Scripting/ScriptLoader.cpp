2aa713fd201f1e10dc0a0903054944a525117447
tabooo <tabagari89@gmail.com>
2014-07-21 20:56:05 +0400

    add boss announce script

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1435,6 +1435,7 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 	void AddSC_Duel_Reset();
+	void AddSC_Boss_Announcer();
 #endif
 
 void AddCustomScripts()
@@ -1442,5 +1443,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 	AddSC_Duel_Reset();
+	AddSC_Boss_Announcer();
 #endif
 }
