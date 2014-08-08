be1a5b568a28b9bb3deef636d2f94230c1f124fa
jackpoz <giacomopoz@gmail.com>
2014-08-08 21:14:12 +0200

    Core/Misc: Fix static analysis issues

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -13615,7 +13615,7 @@ void Player::AddItemToBuyBackSlot(Item* pItem)
                 // found empty
                 if (!m_items[i])
                 {
-                    slot = i;
+                    oldest_slot = i;
                     break;
                 }
 
